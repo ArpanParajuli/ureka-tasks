4503d1111911cc1c98369a5a2c7cf3ac190a3933
ArpanParajuli <arpanparajuli394@gmail.com>
2026-05-22 07:16:38 +0545

    add dependency shadcn

--- Task1/task1/src/layouts/AdminLayouts.tsx
+++ Task1/task1/src/layouts/AdminLayouts.tsx
@@ -1,10 +1,16 @@
-
-
+import {Button} from "@/components/ui/button"
 
 
 function AdminLayouts()
 {
-    return<> <h1>this is admin layout page</h1> </>;
+
+    return(
+        <div className="w-100 h-100 bg-amber-400">
+            <Button>Click Me</Button>
+            <h1>this is admin layout page</h1>
+        </div>
+    
+    );
 }
 
 
